61a1badd170a7885f1941f9ba60fcaaf08897561
Vijay Dohare <vdohare@lsquared.com>
2026-05-26 13:11:02 +0530

    Update test.py

--- code/test.py
+++ code/test.py
@@ -5,11 +5,6 @@ def test_add_1():
     assert test.add(-1, 1) == 0
     assert test.add(0, 0) == 0
 
-def test_subtract():
-    assert test.subtract(5, 2) == 3
-    assert test.subtract(1, -1) == 2
-    assert test.subtract(0, 0) == 0
-
 def test_multiply():
     assert test.multiply(2, 3) == 6
     assert test.multiply(-1, 1) == -1
